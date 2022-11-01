78bfc9d6f8e78579bfbdcf3588999f57aecceb12
Степан Гуляев <stepan.gulyaev.02@gmail.com>
2022-11-01 19:34:47 +0300

    Добавьте файлы проекта.

---
+++ Lexical analyzer/MainWindow.Designer.cs
@@ -0,0 +1,34 @@
+﻿namespace Lexical_analyzer
+    {
+    partial class MainWindow
+        {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputStringBox;
+        private System.Windows.Forms.TextBox result_number;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        }
+    }
+
---
+++ Lexical analyzer/MainWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lexical_analyzer
+    {
+    public partial class MainWindow : Form
+        {
+        public MainWindow()
+            {
+            InitializeComponent();
+            }
+
+        private void InitializeComponent()
+            {
+            this.SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(1790, 900);
+            this.Name = "MainWindow";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+
+            }
+
+        public void createButton(string text, Control parent, Point location, Size size, EventHandler onclick)
+            {
+            Button btn = new Button();
+            this.Controls.Add(btn);
+            btn.Parent = parent;
+            btn.Text = text;
+            btn.Size = size;
+            btn.Location = location;
+            btn.Click += onclick;
+            }
+
+
+        private void MainWindow_Load(object sender, EventArgs e)
+            {
+            createButton("calculate",this,MainWindow);
+            }
+        }
+    }
