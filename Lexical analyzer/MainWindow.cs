3054bd792887b745072e33719194a1ac75734d72
Степан Гуляев <stepan.gulyaev.02@gmail.com>
2022-11-02 19:40:43 +0300

    lexemes list print done need to begin blocks

--- Lexical analyzer/MainWindow.cs
+++ Lexical analyzer/MainWindow.cs
@@ -12,6 +12,29 @@ namespace Lexical_analyzer
     {
     public partial class MainWindow : Form
         {
+        internal static Dictionary<string, long> one_to_nine = new Dictionary<string, long>()
+            {
+                {"one",1},{"two",2},{"three",3},{"four",4},{"five",5},{"six",6},{"seven",7},{"eight",8},
+                {"nine",9}
+            };
+
+        internal static Dictionary<string, long> ten_to_ninteen = new Dictionary<string, long>()
+            {
+                {"ten",10 },{"eleven",11},{"twelwe",12},{"thirteen",13},{"fourteen",14},{"fifteen",15},{"sixteen",16},{"seventeen",17},{"eighteen",18},
+                {"nineteen",19}
+            };
+
+        internal static Dictionary<string, long> tens = new Dictionary<string, long>()
+            {
+                {"twenty",20},{"thirty",30},{"fourty",40},{"fifty",50},{"sixty",60},{"seventy",70},{"eighty",80},
+                {"ninety",90}
+            };
+
+        internal static Dictionary<string, long> rank_words = new Dictionary<string, long>()
+            {
+                {"hundred",100},{"thousand",1000},{"million",1000000},{"billion",1000000000}
+            };
+
         public MainWindow()
             {
             InitializeComponent();
@@ -23,9 +46,29 @@ namespace Lexical_analyzer
 
         private void calculateButton_Click(object sender, EventArgs e)
             {
-            string inputString = inputStringBox.Text;
-            Lexeme lexeme = new Lexeme(inputString);
+            string input = inputStringBox.Text;
+            calculate(input);
+            }
 
+        private void calculate(string input)
+            {
+            string[] words = input.Split(' ');
+            List<Lexeme> lexes = new List<Lexeme>();
+            foreach (string word in words)
+                {
+                Lexeme lex = new Lexeme(word);
+                if(lex.input_error())
+                    {
+                    MessageBox.Show("Одно из слов введено неверно", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    numberBox.Clear();
+                    lex = null;
+                    break;
+                    }
+                else
+                    {
+                    lexes.Add(lex);
+                    }
+                }
             }
         }
     }
--- Lexical analyzer/lexicalClasses/Lexeme.cs
+++ Lexical analyzer/lexicalClasses/Lexeme.cs
@@ -12,31 +12,69 @@ namespace Lexical_analyzer
         {
         public string lexeme;
         public string type;
-        public string value;
+        public long value;
 
         public Lexeme(string inputStr)
             {
             lexeme = inputStr;
-            type = getType(inputStr);
+            type = getType(lexeme);
+            value = getValue(lexeme);
             }
 
         public string getType(string lexeme)
             {
-            type = lexeme;
-            var dictionaryList = typeof(Dictionaries).GetFields(BindingFlags.Static | BindingFlags.NonPublic);
-            var dictionaryListNames = Array.ConvertAll(dictionaryList, field => field.Name);
-            /*foreach (var name in dictionaryList)
+            string typeStr = "undefined";
+            if (MainWindow.one_to_nine.ContainsKey(lexeme))
                 {
-                if (Dictionaries.)
-                    {
-
-                    }
+                typeStr = "one_to_nine";
                 }
-            if (Dictionaries.one_to_nine.ContainsKey(lexeme))
+            else if (MainWindow.ten_to_ninteen.ContainsKey(lexeme))
                 {
-                return 
-                }*/
-            return lexeme;
+                typeStr = "ten_to_ninteen";
+                }
+            else if (MainWindow.tens.ContainsKey(lexeme))
+                {
+                typeStr = "tens";
+                }
+            else if (MainWindow.rank_words.ContainsKey(lexeme))
+                {
+                typeStr = "rank_words";
+                }
+            return typeStr;
+            }
+
+        public long getValue(string lexeme)
+            {
+            long value = 0;
+            if (MainWindow.one_to_nine.ContainsKey(lexeme))
+                {
+                value = MainWindow.one_to_nine[lexeme];
+                }
+            else if (MainWindow.ten_to_ninteen.ContainsKey(lexeme))
+                {
+                value = MainWindow.ten_to_ninteen[lexeme];
+                }
+            else if (MainWindow.tens.ContainsKey(lexeme))
+                {
+                value = MainWindow.tens[lexeme];
+                }
+            else if (MainWindow.rank_words.ContainsKey(lexeme))
+                {
+                value = MainWindow.rank_words[lexeme];
+                }
+            return value;
+            }
+
+        public bool input_error()
+            {
+            if (type == "undefined")
+                {
+                return true;
+                }
+            else
+                {
+                return false;
+                }
             }
         }
     }
