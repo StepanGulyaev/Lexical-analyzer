47aa4846c68557a7f074ac42a8b5a1695fb4ce84
Степан Гуляев <stepan.gulyaev.02@gmail.com>
2022-12-16 13:54:26 +0300

    multiple spaces between words fix

--- Lexical analyzer/MainWindow.cs
+++ Lexical analyzer/MainWindow.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Lexical_analyzer
@@ -47,7 +43,7 @@ namespace Lexical_analyzer
 
         private void calculateButton_Click(object sender, EventArgs e)
             {
-            string input = inputStringBox.Text.Trim();
+            string input = Regex.Replace(inputStringBox.Text.Trim(), @"\s+", " ");
             calculate(input);
             }
 
